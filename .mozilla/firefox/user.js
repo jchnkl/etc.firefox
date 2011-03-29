65f516ca6d2ddfa96d61a802a86466f47f63850c
Jochen Keil <jochen.keil@gmail.com>
2011-03-29 09:14:58 +0200

    firefox: disable animated zoom for panorama; do not steal focus

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -1,2 +1,5 @@
+user_pref("browser.panorama.animate_zoom", false);
+user_pref("browser.tabs.loadBookmarksInBackground", true);
+user_pref("browser.tabs.loadDivertedInBackground", true);
 user_pref("capability.policy.policynames", "nofocus");
 user_pref("capability.policy.nofocus.sites", "http://www.google.com http://dict.leo.org");
