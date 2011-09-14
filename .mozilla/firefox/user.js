c9e6e65489c8fb49d21abb32cca8a5a6924187d2
Jochen Keil <jochen.keil@gmail.com>
2011-09-14 20:38:38 +0200

    update nofocus sites and policy

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -1,5 +1,7 @@
 user_pref("browser.panorama.animate_zoom", false);
 user_pref("browser.tabs.loadBookmarksInBackground", true);
 user_pref("browser.tabs.loadDivertedInBackground", true);
+
 user_pref("capability.policy.policynames", "nofocus");
-user_pref("capability.policy.nofocus.sites", "http://www.google.com http://dict.leo.org");
+user_pref("capability.policy.nofocus.sites", "http://www.google.com http://www.google.de http://dict.leo.org");
+user_pref("capability.policy.nofocus.HTMLInputElement.focus", "noAccess");
